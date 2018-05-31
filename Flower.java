b4682624747b92445709067a6bfdb7afac3fe8cf
Nattamon <59160877@go.buu.ac.th>
2018-05-31 15:08:25 +0700

    Lab 1 JAVA

--- Flower.java
+++ Flower.java
@@ -1,30 +1,32 @@
-import java.util.Scanner;
-
 public class Flower {
-	Scanner kb = new Scanner (System.in);
 
 	String name;
 	int flower;
 	float price;
 	
+	public Flower (String name,int flower,float price) {
+		this.name =name;
+		this.flower = flower;
+		this.price = price;
+	}
 
 	public String getName() {
 		return this.name;
 	}
 	public void setName(String name) {
-		this.name = kb.next();
+		this.name = name;
 	}
 	public int getFlower() {
 		return this.flower;
 	}
 	public void setFlower(int flower) {
-		this.flower = kb.nextInt();
+		this.flower = flower;
 	}
 	public float getPrice() {
 		return this.price;
 	}
 	public void setPrice(float price) {
-		this.price = kb.nextFloat();
+		this.price = price;
 	}
 	
 	
